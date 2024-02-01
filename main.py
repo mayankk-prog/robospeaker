24586f7918e80ff536873752b0180c028e0b4f3e
mayankk-prog <bhmayank001@gmail.com>
2024-02-01 12:56:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,3 @@ while True:
     x= input("Enter what you want oto speak")
     command=    "  PowerShell -Command \"Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{}');\" ".format(x)
     os.system(command)
-
-'''
-import os
-
-while True:
-    text = input("Type something to speak aloud (or 'quit' to exit): ")
-    if text.lower() == "quit":
-        break
-
-    # Use PowerShell to invoke text-to-speech
-    os.system("PowerShell -Command \"Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{}');\"".format(text))
-'''
